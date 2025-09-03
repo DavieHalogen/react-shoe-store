211f6cdd380676bb5d2e70cc2113bed8ae4705c5
DavieHalogen <daviehalogen@gmail.com>
2025-09-03 23:23:55 +0300

    Update apiService.js

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://shoestore.serveo.net/api';
+const API_URL = 'https://react-shoe-store.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
