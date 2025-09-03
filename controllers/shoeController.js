7c04f0eae6c9e4c10f745c48e65e5ca67ff75c74
DavieHalogen <daviehalogen@gmail.com>
2025-09-04 01:03:06 +0300

    Update shoeController.js for postgres

--- controllers/shoeController.js
+++ controllers/shoeController.js
@@ -1,6 +1,6 @@
-const { pool } = require('../config/db');
+const pool = require('../config/db');
+const cloudinary = require('../config/cloudinary');
 
-// Get all shoes
 exports.getShoes = async (req, res) => {
     try {
         const result = await pool.query('SELECT * FROM "Shoes"');
@@ -10,24 +10,25 @@ exports.getShoes = async (req, res) => {
     }
 };
 
-// Create a new shoe
 exports.createShoe = async (req, res) => {
     const { name, price } = req.body;
-    const image = req.file ? req.file.filename : null;
-    if (!image) return res.status(400).json({ message: 'Image is required' });
+    if (!req.file) return res.status(400).json({ message: 'Image is required' });
 
     try {
+        const upload = await cloudinary.uploader.upload(req.file.path, { folder: 'shoestore/shoes' });
+        const imageUrl = upload.secure_url;
+
         const result = await pool.query(
             'INSERT INTO "Shoes" (name, price, image) VALUES ($1, $2, $3) RETURNING id',
-            [name, price, image]
+            [name, price, imageUrl]
         );
-        res.status(201).json({ id: result.rows[0].id, name, price, image });
+
+        res.status(201).json({ id: result.rows[0].id, name, price, image: imageUrl });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 };
 
-// Get a shoe by ID
 exports.getShoeById = async (req, res) => {
     const { id } = req.params;
     try {
@@ -39,23 +40,22 @@ exports.getShoeById = async (req, res) => {
     }
 };
 
-// Update a shoe by ID
 exports.updateShoe = async (req, res) => {
     const { id } = req.params;
     const { name, price } = req.body;
-    const image = req.file ? req.file.filename : null;
 
     try {
         let query = 'UPDATE "Shoes" SET name = $1, price = $2';
         const params = [name, price];
 
-        if (image) {
-            query += ', image = $3';
-            params.push(image);
+        if (req.file) {
+            const upload = await cloudinary.uploader.upload(req.file.path, { folder: 'shoestore/shoes' });
+            query += ', image = $3 WHERE id = $4';
+            params.push(upload.secure_url, id);
+        } else {
+            query += ' WHERE id = $3';
+            params.push(id);
         }
-
-        query += ' WHERE id = $4';
-        params.push(id);
 
         const result = await pool.query(query, params);
 
@@ -66,7 +66,6 @@ exports.updateShoe = async (req, res) => {
     }
 };
 
-// Delete a shoe by ID
 exports.deleteShoe = async (req, res) => {
     const { id } = req.params;
     try {
