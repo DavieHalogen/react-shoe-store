1e5726e9d8fa5df304b4bf52fef3be2bf66bdf34
DavieHalogen <daviehalogen@gmail.com>
2025-09-04 02:16:31 +0300

    Update shoeController.js

--- controllers/shoeController.js
+++ controllers/shoeController.js
@@ -1,4 +1,4 @@
-const pool = require('../config/db');
+const { pool } = require('../config/db');
 const cloudinary = require('../config/cloudinary');
 
 // Helper function to upload buffer to Cloudinary
